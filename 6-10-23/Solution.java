3ade2a8bf8e08e268694345fa1930b6dfe2244f5
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-11 00:11:49 +0500

    changes

---
+++ 6-10-23/Solution.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Hello {
+
+  public void mapper(String a) {
+
+  }
+
+  public static void main(String[] args) {
+   //solution will be uploaded soon
+  }
+}
